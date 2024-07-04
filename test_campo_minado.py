025025e8bdcd98f1c836258346b0cabc21274766
AngeloFrighetto <angelofrighetto100@gmail.com>
2024-07-04 18:34:38 -0300

    Feito uma nova função para contar as bombas vizinhas

--- test_campo_minado.py
+++ test_campo_minado.py
@@ -1,5 +1,5 @@
 import unittest
-from jogo import criar_tabuleiro
+import criar_tabuleiro, contar_bombas_vizinhas, revelar_tabuleiro, processar_ranking # type: ignore
 
 class TestCampoMinado(unittest.TestCase):
 
@@ -10,3 +10,12 @@ class TestCampoMinado(unittest.TestCase):
         self.assertEqual(len(tabuleiro[0]), colunas)
         bombas_count = sum(row.count('💣') for row in tabuleiro)
         self.assertEqual(bombas_count, bombas)
+    def test_contar_bombas_vizinhas(self):
+        tabuleiro = [
+            ['🟦', '🟦', '🟦'],
+            ['🟦', '💣', '🟦'],
+            ['🟦', '🟦', '🟦']
+        ]
+        self.assertEqual(contar_bombas_vizinhas(tabuleiro, 0, 0), 1)
+        self.assertEqual(contar_bombas_vizinhas(tabuleiro, 1, 1), 0)
+        self.assertEqual(contar_bombas_vizinhas(tabuleiro, 2, 2), 1)
